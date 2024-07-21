7d5971c58341418308faf316c02175d98439d1bd
Fahfouhi <fahfouhi@gmail.com>
2024-07-21 02:59:26 +0200

    Initial commit: Set up front-end and back-end projects for TaskManagerPro app

---
+++ TaskMangerPro-backend/src/main/java/ma/ibra/taskmangerprobackend/TaskMangerProBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.ibra.taskmangerprobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMangerProBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMangerProBackendApplication.class, args);
+    }
+
+}
---
+++ TaskMangerPro-backend/src/test/java/ma/ibra/taskmangerprobackend/TaskMangerProBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ibra.taskmangerprobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMangerProBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
